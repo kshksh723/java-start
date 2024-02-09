2a47f49274c3d5294327a75757a731e34bb1022e
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-02-09 15:53:32 +0900

    ddf

---
+++ src/variable/Var6.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+       // int a ;
+      //  System.out.println(a);
+        // 변수를 선언하지 않으면 오류가 뜬다
+    }
+}
+
+// 지역변수는 개발자가 직접 초기화를 해주어야 한다
