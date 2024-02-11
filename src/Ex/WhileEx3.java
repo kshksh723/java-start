512cb4447fffe7bc8631d71eddef1c59291d15ed
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-02-11 14:08:14 +0900

    2/11

---
+++ src/Ex/WhileEx3.java
@@ -0,0 +1,16 @@
+package Ex;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+       int max = 100;
+       int sum = 0;
+       int i = 1;
+       while(i <= max){
+           sum += i; //sum = sum + i
+           i++;// i= i + 1
+       }
+        System.out.println(sum);
+
+
+    }
+}
---
+++ src/loop/DoWhile1.java
@@ -0,0 +1,20 @@
+package loop;
+/*
+- do - while문 구조
+do {
+        //코드
+    } while (조건식);
+
+ */
+public class DoWhile1 {
+
+    public static void main(String[] args) {
+        int i = 10;
+
+        while (i<3){
+            System.out.println("현재 숫자는:" + i);
+            i++;
+        }
+    }
+}
+// i=10이기 때문에 while(i<3) 조건식이 거짓이 된다 아무것도 출력되지 않는다
